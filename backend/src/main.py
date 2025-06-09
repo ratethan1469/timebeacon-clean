9f2397881160aa9bd7805eb4b8094b687875b9c2
Miriam Ritter <miriamritter@Miriams-Air-2.lan>
2025-06-09 18:03:48 -0400

    Describe your changes here

--- backend/src/main.py
+++ backend/src/main.py
@@ -200,9 +200,8 @@ def create_time_entry(event, integration):
     return True
 
 @app.route('/')
-def serve_index():
-    root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
-    return send_from_directory(root_dir, 'index.html')
+def index():
+    return render_template('index.html')
 
 @app.route('/timehub')
 def timehub():
