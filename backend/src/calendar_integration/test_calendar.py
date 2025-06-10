43888d18d8846fbbeb9438f873fb1f3991e970ad
ratethan1469 <ethan.ratnowsky@gmail.com>
2025-06-09 20:12:47 -0400

    Update test_calendar.py

--- backend/src/calendar_integration/test_calendar.py
+++ backend/src/calendar_integration/test_calendar.py
@@ -1,5 +1,5 @@
+print("Test script started")
 from google_calendar import GoogleCalendar
 
-# Use your actual credentials file
 calendar = GoogleCalendar('senku-calendar-b2c2beb40b2c.json')
 print("Success! GoogleCalendar object created.")
